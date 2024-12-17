354c7ecd11ec0d909392188b00d88e5d8ebb0772
Robert Vokac <robertvokac@robertvokac.com>
2024-12-17 12:44:04 +0100

    Changes

---
+++ src/main/java/com/openeggbert/jxna/Microsoft/Xna/Framework/Rectangle.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.openeggbert.jxna.Microsoft.Xna.Framework;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author robertvokac
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+public class Rectangle extends com.openeggbert.jdotnet.System.Struct<Rectangle> {
+    
+    private static final Rectangle emptyRectangle = new Rectangle().reset();
+    
+    public int X;
+
+    public int Y;
+
+    public int Width;
+
+    public int Height;
+
+    @Override
+    public Rectangle copy() {
+        return new Rectangle(X, Y, Width, Height);
+    }
+
+    @Override
+    public Rectangle reset() {
+        X = 0;
+        Y = 0;
+        Width = 0;
+        Height = 0;
+        return this;
+    }
+    
+    public int Left() { return X;}
+
+    public int Right() { return  X + Width;}
+
+    public int Top() { return  Y;}
+
+    public int Bottom() { return  Y + Height;}
+
+    
+}
---
+++ src/main/java/com/openeggbert/jxna/Microsoft/Xna/Framework/TitleContainer.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.openeggbert.jxna.Microsoft.Xna.Framework;
+
+import com.openeggbert.jdotnet.System.IO.Stream;
+
+/**
+ *
+ * @author robertvokac
+ */
+public class TitleContainer {
+    public static Stream OpenStream(String name) {
+        return null;//todo
+    }
+    
+}
